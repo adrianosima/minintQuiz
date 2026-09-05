02ce9d5753f62eff424ea5df9ab5f7efb8e27d93
Adriano Simão <adrianomiguelsimao2@gmail.com>
2026-09-06 00:29:08 +0100

    actualização

--- assets/script.js
+++ assets/script.js
@@ -2,36 +2,36 @@ const questions =[
     {
         question: 'Em que continente está localizada Angola?',
         answers: [
-            {id:1, text: 'Tubarão', correct: false},
-            {id:2, text: 'Baleia Azul', correct: false},
-            {id:3, text: 'Angola está localizada no continente africano, na região da África Austral.', correct: true},
-            {id:4, text: 'Elefante', correct: false}
+            {id:1, text: 'continente Europel', correct: false},
+            {id:2, text: 'continente Americano', correct: false},
+            {id:3, text: ' continente africano, na região da África Austral.', correct: true},
+            {id:4, text: 'continente Australiano', correct: false}
         ]
     },
     {
         question: 'Qual é a capital de Angola?',
         answers: [
-            {id:1, text: 'Tubarão', correct: false},
-            {id:2, text: 'Baleia Azul', correct: false},
-            {id:3, text: 'girafa', correct: false},
+            {id:1, text: 'Huambo', correct: false},
+            {id:2, text: 'Benguela', correct: false},
+            {id:3, text: 'Malange', correct: false},
             {id:4, text: 'Luanda', correct: true}
         ]
     },
     {
         question: 'Quais países fazem fronteira terrestre com Angola?',
         answers: [
-            {id:1, text: 'Tubarão', correct: false},
+            {id:1, text: 'República Democrática do Congo, República do Congo, Zâmbia e Cuba', correct: false},
             {id:2, text: ' República Democrática do Congo, República do Congo, Zâmbia e Namíbia', correct: true},
-            {id:3, text: 'girafa', correct: false},
-            {id:4, text: 'Elefante', correct: false}
+            {id:3, text: 'Gabão, República do Congo, Zâmbia e Namíbia', correct: false},
+            {id:4, text: 'República Democrática do Congo, República do Congo, Africa do sul e Namíbia', correct: false}
         ]
     },
     {
         question: 'Quando foi proclamada a Independência Nacional de Angola?',
         answers: [
-            {id:1, text: 'Tubarão', correct: false},
-            {id:2, text: 'Baleia Azul', correct: false},
-            {id:3, text: 'girafa', correct: false},
+            {id:1, text: '12 de Dezembro de 1977', correct: false},
+            {id:2, text: '12 de Fevereiro de 1976', correct: false},
+            {id:3, text: '11 de Novembro de 1976', correct: false},
             {id:4, text: ' 11 de Novembro de 1975', correct: true}
         ]
     },
@@ -39,90 +39,90 @@ const questions =[
         question: 'Quem proclamou a Independência de Angola?',
         answers: [
             {id:1, text: 'António Agostinho Neto.', correct: true},
-            {id:2, text: 'Baleia Azul', correct: false},
-            {id:3, text: 'girafa', correct: false},
-            {id:4, text: 'Elefante', correct: false}
+            {id:2, text: 'Lucio Lara', correct: false},
+            {id:3, text: 'Jonas Savimbe', correct: false},
+            {id:4, text: 'Jose Eduardo dos santos', correct: false}
         ]
     },
     {
         question: 'Qual era o nome oficial de Angola durante o período colonial?',
         answers: [
-            {id:1, text: 'Tubarão', correct: false},
-            {id:2, text: 'Baleia Azul', correct: false},
-            {id:3, text: 'Angola está localizada no continente africano, na região da África Austral.', correct: false},
+            {id:1, text: 'Republica de Angola', correct: false},
+            {id:2, text: 'Colonia Angolana', correct: false},
+            {id:3, text: 'Província de Angola', correct: false},
             {id:4, text: 'Província Ultramarina de Angola ', correct: true}
         ]
     },
     {
         question: 'Durante aproximadamente quanto tempo Angola esteve sob domínio colonial português?',
         answers: [
-            {id:1, text: 'Tubarão', correct: false},
-            {id:2, text: 'Baleia Azul', correct: false},
+            {id:1, text: 'Durante 20 anos', correct: false},
+            {id:2, text: 'Menos de 150 anos, até à proclamação da Independência em 1975', correct: false},
             {id:3, text: 'Durante vários séculos, até à proclamação da Independência em 1975', correct: true},
-            {id:4, text: 'Elefante', correct: false}
+            {id:4, text: 'Menos de 100 anos, até à proclamação da Independência em 1975', correct: false}
         ]
     },
     {
         question: 'Quais foram os principais movimentos de libertação nacional de Angola?',
         answers: [
-            {id:1, text: 'Tubarão', correct: false},
-            {id:2, text: 'Baleia Azul', correct: false},
+            {id:1, text: '•MPLA; •FNLA;  •CASA-C;', correct: false},
+            {id:2, text: '•MPLA;•UNITA; •CASA-C;', correct: false},
             {id:3, text: '•MPLA; •FNLA; •UNITA;', correct: true},
-            {id:4, text: 'Elefante', correct: false}
+            {id:4, text: ' •FNLA; •UNITA; •CASA-C;', correct: false}
         ]
     },
     {
         question: ' O que significa MPLA?',
         answers: [
-            {id:1, text: 'Tubarão', correct: false},
-            {id:2, text: 'Baleia Azul', correct: false},
-            {id:3, text: 'Angola está localizada no continente africano, na região da África Austral.', correct: false},
+            {id:1, text: 'Movimento Popular de Angola.', correct: false},
+            {id:2, text: 'Movimento Popular da Luta de Libertação de Angola.', correct: false},
+            {id:3, text: 'Movimento de Libertação de Angola.', correct: false},
             {id:4, text: ' Movimento Popular de Libertação de Angola.', correct: true}
         ]
     },
     {
         question: ' O que significa FNLA?',
         answers: [
-            {id:1, text: 'Tubarão', correct: false},
-            {id:2, text: 'Baleia Azul', correct: false},
+            {id:1, text: 'Frente Nacional  de Angola', correct: false},
+            {id:2, text: 'Frente Nacional da Luta de Libertação de Angola', correct: false},
             {id:3, text: 'Frente Nacional de Libertação de Angola', correct: true},
-            {id:4, text: 'Elefante', correct: false}
+            {id:4, text: 'Frente  de Libertação de Angola', correct: false}
         ]
     },
     {
         question: 'O que significa UNITA?',
         answers: [
             {id:1, text: 'União Nacional para a Independência Total de Angola.', correct: true},
-            {id:2, text: 'Baleia Azul', correct: false},
-            {id:3, text: 'Angola está localizada no continente africano, na região da África Austral.', correct: false},
-            {id:4, text: 'Elefante', correct: false}
+            {id:2, text: 'União  para a Independência Total de Angola.', correct: false},
+            {id:3, text: 'União Nacional para a Independêncide Angola.', correct: false},
+            {id:4, text: 'União Nacional de Angola.', correct: false}
         ]
     },
     {
         question: 'Quem foi o primeiro Presidente de Angola?',
         answers: [
             {id:1, text: ' António Agostinho Neto.', correct: true},
-            {id:2, text: 'Baleia Azul', correct: false},
-            {id:3, text: 'Angola está localizada no continente africano, na região da África Austral.', correct: false},
-            {id:4, text: 'Elefante', correct: false}
+            {id:2, text: 'jose Eduardo Dos Santos', correct: false},
+            {id:3, text: 'Jonas Savimbe', correct: false},
+            {id:4, text: 'Lucio Lara', correct: false}
         ]
     },
     {
         question: 'Quando começou a luta armada de libertação nacional em Angola?',
         answers: [
-            {id:1, text: 'Tubarão', correct: false},
-            {id:2, text: 'A luta armada é geralmente situada a partir de 4 de Fevereiro de 1961, data associada ao início da luta armada de libertação nacional.', correct: true},
-            {id:3, text: 'Angola está localizada no continente africano, na região da África Austral.', correct: false},
-            {id:4, text: 'Elefante', correct: false}
+            {id:1, text: '04 de Março de 1961', correct: false},
+            {id:2, text: '4 de Fevereiro de 1961', correct: true},
+            {id:3, text: '4 de Fevereiro de 1975', correct: false},
+            {id:4, text: '5 de Fevereiro de 1961', correct: false}
         ]
     },
     {
         question: 'O que aconteceu em 4 de Fevereiro de 1961?',
         answers: [
-            {id:1, text: 'Tubarão', correct: false},
+            {id:1, text: 'Ocorreram despaches contra estabelecimentos coloniais em Luanda, data que passou a ser considerada um marco do início da luta armada de libertação nacional em Angola', correct: false},
             {id:2, text: 'Ocorreram ataques contra estabelecimentos coloniais em Luanda, data que passou a ser considerada um marco do início da luta armada de libertação nacional em Angola', correct: true},
-            {id:3, text: 'Angola está localizada no continente africano, na região da África Austral.', correct: false},
-            {id:4, text: 'Elefante', correct: false}
+            {id:3, text: 'Jonas Savimbe começou a bombardear a unita', correct: false},
+            {id:4, text: 'Ocorreram ataques contra estabelecimentos coloniais em Malange, data que passou a ser considerada um marco do início da luta armada de libertação nacional em Angola', correct: false}
         ]
     },
     {
